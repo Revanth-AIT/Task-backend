310913b798fc59ade3fb345bd2bf3889898eadfd
Revanth-AIT <revanthait03@gmail.com>
2025-06-13 12:56:26 +0530

    Update cron.service.ts

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { QueueProducerService } from './queues/queue.producer.service'; // ✅
+import { QueueProducerService } from './queues/queue.producer.service'; 
 
 @Injectable()
 export class CronService {
